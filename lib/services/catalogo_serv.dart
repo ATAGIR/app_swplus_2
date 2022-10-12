e7d6878ccf61d375e5dcc696d9a13b8c7b8ca7b8
IsraRM93 <87777674+IsraRM93@users.noreply.github.com>
2022-10-12 09:30:27 -0500

    CatService

--- lib/services/catalogo_serv.dart
+++ lib/services/catalogo_serv.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: avoid_print, use_build_context_synchronously, dead_code_catch_following_catch
 
 import 'package:dio/dio.dart';
@@ -8,17 +7,15 @@ import 'package:telemetria/models/models.dart';
 import 'package:telemetria/services/errores.dart';
 import '../utils/message.dart';
 
-
 class CatService {
-
   static final CatService _catService = CatService._internal();
   final Dio _dio = Dio();
   String token = '';
 
-  factory CatService(){
+  factory CatService() {
     return _catService;
   }
-  CatService._internal(){
+  CatService._internal() {
     _dio.options.baseUrl = 'http://infopro-api.swplus.com.mx/api/log/get_last';
     _dio.options.connectTimeout = 120000;
     _dio.options.receiveTimeout = 120000;
@@ -26,11 +23,9 @@ class CatService {
     _dio.options.responseType = ResponseType.json;
   }
 
-
-
   Future<MedidorUser?> getLast(BuildContext context, String token) async {
-    final medidoruser = Provider.of<MedidorUser>(context, listen: false);
     try {
+      final medidoruser = Provider.of<MedidorUser>(context, listen: false);
       Message.show(context);
       final response = await _dio.request('medidor',
           data: {"token": token}, options: Options(method: 'GET'));
