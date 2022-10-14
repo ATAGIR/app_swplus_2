6464ec6c89cce7124c12067d5879833fd54b0aae
Aketzali Aguilar <aketzatrejo104@gmail.com>
2022-10-14 11:07:15 -0500

    mapac

--- lib/api/configure_api.dart
+++ lib/api/configure_api.dart
@@ -10,6 +10,10 @@ class ConfigureApi {
   ConfigureApi._internal();
   static const base_url = "http://infopro-api.swplus.com.mx/api/log/";
 
+  static const String mapBoxUrl = "https://api.mapbox.com/styles/v1/swplus-tecnologias/cl97725qz001t14qlh4h5el6d/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3dwbHVzLXRlY25vbG9naWFzIiwiYSI6ImNsOGtheXV3aDBhbW80MHBpbWdncHFjM2YifQ.G3Yg7SECTj4nvAAoK4DuaA";
+  static const String mapBoxAccessToken = 'TOKEN';
+  static const String mapBoxStyleId = 'ID';
+
   static BaseOptions options = BaseOptions(
     baseUrl: base_url,
     connectTimeout: 120000,
