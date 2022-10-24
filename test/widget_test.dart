18c7e20ab85716427fdc9350e3eea84a0b877754
Israel <israelriveramendoza@outlook.com>
2022-10-24 13:29:49 -0500

    RIMEIS 24102022 1329

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:telemetria/main.dart';
-import 'package:telemetria/pages/verlista.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
