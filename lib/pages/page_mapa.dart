8c577837c94f85659138bd018f1e09eb965cc238
Israel <israelrivera718@gmail.com>
2022-10-26 12:22:55 -0500

    RIMEIS 26102022 1221

--- lib/pages/page_mapa.dart
+++ lib/pages/page_mapa.dart
@@ -1,32 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:telemetria/models/map_detail.dart';
+import 'package:telemetria/providers/login_prov.dart';
 import 'package:telemetria/utils/responsive.dart';
-import '../widget/ubicacion.dart';
+import 'package:telemetria/widget/ubicacion.dart';
+
+Future<MapDetail>? _detalleMap;
+List<MapDetail>? listaDetalleMap;
+bool emptyArray = true;
+String? itemSelected;
 
 class PageMapa extends StatefulWidget {
   static const routeName = 'PageMapa';
-  const PageMapa(this.latitud, this.longitud, {super.key});
-
+  const PageMapa(this.latitud, this.longitud, this.nsut, this.etiqueta, {super.key});
   final double? latitud;
   final double? longitud;
+  final String? nsut;
+  final String? etiqueta;
 
   @override
   State<PageMapa> createState() => _PageMapaState();
 }
 
 class _PageMapaState extends State<PageMapa> {
+  MapDetail? detalleActual;
+
+  double? get longitud => null;
+  @override
+  void initState() {
+    super.initState();
+    final loginProvider = Provider.of<LoginProvider>(context, listen: false);
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     final responsive = Responsive(context);
     return SafeArea(
       child: Scaffold(
         // ignore: avoid_unnecessary_containers
-        body: Container(
-          height: responsive.hp(45),
-          child: Ubicacion(
-              minZoom: 5,
-              maxZoom: 20,
-              latitud: widget.latitud!,
-              longitud: widget.longitud!),
+        body: GestureDetector(
+          onTap: () {
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+            }
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: responsive.hp(45),
+                  child: Ubicacion(
+                      minZoom: 5,
+                      maxZoom: 20,
+                      latitud: widget.latitud!,
+                      longitud: widget.longitud!),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
