511518cf218e740a055f273075ec13c6b2a98a5c
adrianPacha <103062536+AdrianSWPlus@users.noreply.github.com>
2022-10-31 16:53:36 -0600

    GUIAA 31102022 1653

--- lib/pages/registro_login.dart
+++ lib/pages/registro_login.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:telemetria/services/registro_serv.dart';
 import 'package:telemetria/utils/caracteres.dart';
+import 'package:telemetria/utils/message.dart';
 import '../utils/responsive.dart';
 import 'login.dart';
-
-
 
 class RegistroLogin extends StatefulWidget {
   const RegistroLogin({Key? key}) : super(key: key);
@@ -25,57 +25,50 @@ class _RegistroLoginState extends State<RegistroLogin> {
 
     return SafeArea(
       child: Scaffold(
-        body: GestureDetector(
-           onTap: (){
-            FocusScopeNode currentFocus =
-              FocusScope.of(context);
-            if (!currentFocus.hasPrimaryFocus){
-              currentFocus.unfocus();
-            }
-          },
-          child: SingleChildScrollView(
-            child: Center(
-              child: Form(
-
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  children: [
-                    Image.asset('assets/imagenes/logo.jpeg',
+          body: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: SingleChildScrollView(
+          child: Center(
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/imagenes/logo.jpeg',
                     height: responsive.hp(30),
                     width: responsive.wp(65),
-                    ),
-                    _userTextField(),
-                      SizedBox(height: Responsive(context).wp(5)
-                      ),
-                      _emailTextField(),
-                      SizedBox(height: Responsive(context).wp(5)
-                      ),
-                      _passwordTextField(),
-                      SizedBox(height: Responsive(context).wp(5)
-                      ),
-                       _passwordCTextField(),
-                      SizedBox(height: Responsive(context).wp(5)
-                      ),
-                          _botonRegistrarse(),
-                  ],
-                ),
+                  ),
+                  _userTextField(),
+                  SizedBox(height: Responsive(context).wp(5)),
+                  _emailTextField(),
+                  SizedBox(height: Responsive(context).wp(5)),
+                  _passwordTextField(),
+                  SizedBox(height: Responsive(context).wp(5)),
+                  _passwordCTextField(),
+                  SizedBox(height: Responsive(context).wp(5)),
+                  _botonRegistrarse(),
+                ],
               ),
             ),
           ),
-        )
-      ),
+        ),
+      )),
     );
   }
-  
+
   Widget _userTextField() {
     return StreamBuilder(
-      builder: (BuildContext context, AsyncSnapshot snapshot){
-        return Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: Responsive(context).hp(4)),
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: Responsive(context).hp(4)),
+        child: TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(50))),
             icon: Icon(Icons.person),
@@ -85,113 +78,116 @@ class _RegistroLoginState extends State<RegistroLogin> {
           onChanged: (text) {
             username = text;
           },
-          validator: (text){
-            if (text!.trim().length < 5){
+          validator: (text) {
+            if (text!.trim().length < 5) {
               return 'Usuario invalido';
             }
             return null;
           },
-          ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
-  
+
   Widget _emailTextField() {
     return StreamBuilder(
-      builder: (BuildContext context, AsyncSnapshot snapshot){
-        return Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: Responsive(context).hp(4)),
-          child: TextFormField(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: Responsive(context).hp(4)),
+        child: TextFormField(
             keyboardType: TextInputType.emailAddress,
             decoration: const InputDecoration(
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(50))),
-            icon: Icon(Icons.email),
-            hintText: 'Ingrese correo electronico',
-            labelText: 'Correo electronico',
-          ),
-          onChanged: (value) => email = value.trim(),
-          validator: (value){
-            return Caracteres().validaCorreo(value!);
-          }
-          ),
-        );
-      }
-    );
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              icon: Icon(Icons.email),
+              hintText: 'Ingrese correo electronico',
+              labelText: 'Correo electronico',
+            ),
+            onChanged: (value) => email = value.trim(),
+            validator: (value) {
+              return Caracteres().validaCorreo(value!);
+            }),
+      );
+    });
   }
-  
+
   Widget _passwordTextField() {
     return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: Responsive(context).hp(4)),
+        padding: EdgeInsets.symmetric(horizontal: Responsive(context).hp(4)),
         child: TextFormField(
-          keyboardType: TextInputType.emailAddress,
-          obscureText: true,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(50))),
-            icon: Icon(Icons.lock_rounded),
-            hintText: 'Ingrese una contraseña',
-            labelText: 'Contraseña',
-          ),
-          onChanged: (value) => password = value.trim(),
-          validator: (value){
-            if (value != null && value.length >=5){
-              return null;
-            } else {
-              return 'Verifique su contraseña';
-            }
-          }
-          ),
-        );
-      }
-    );
+            keyboardType: TextInputType.emailAddress,
+            obscureText: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              icon: Icon(Icons.lock_rounded),
+              hintText: 'Ingrese una contraseña',
+              labelText: 'Contraseña',
+            ),
+            onChanged: (value) => password = value.trim(),
+            validator: (value) {
+              if (value != null && value.length >= 5) {
+                return null;
+              } else {
+                return 'Verifique su contraseña';
+              }
+            }),
+      );
+    });
   }
-  
+
   Widget _passwordCTextField() {
     return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: Responsive(context).hp(4)),
+        padding: EdgeInsets.symmetric(horizontal: Responsive(context).hp(4)),
         child: TextFormField(
-          keyboardType: TextInputType.emailAddress,
-          obscureText: true,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(50))),
-            icon: Icon(Icons.lock_rounded),
-            hintText: 'Confirmar contraseña',
-            labelText: 'Confirmar contraseña',
-          ),
-          onChanged: (value) => confirmarPassword = value.trim(),
-          validator: (value){
-            if (value != null && value.length >= 5){
-              return null;
-            } else{
-              return 'Verifique su contraseña';
-            }
-          }
-          ),
-        );
-      }
-    );
+            keyboardType: TextInputType.emailAddress,
+            obscureText: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              icon: Icon(Icons.lock_rounded),
+              hintText: 'Confirmar contraseña',
+              labelText: 'Confirmar contraseña',
+            ),
+            onChanged: (value) => confirmarPassword = value.trim(),
+            validator: (value) {
+              if (value != null && value.length >= 5) {
+                return null;
+              } else {
+                return 'Verifique su contraseña';
+              }
+            }),
+      );
+    });
   }
-  
+
   Widget _botonRegistrarse() {
-   return ElevatedButton(
+    return ElevatedButton(
       style:
           ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),
       onPressed: () {
-        Navigator.pop(context, MaterialPageRoute(builder: (context)=>  const Login()));
-
+        if (username.isEmpty) {
+          Message.showMessage(
+              context: context,
+              message:
+                  '  Se require completar la información solicitada, verifique',
+              color: Color(0xffBF4045));
+          return;
+        }
+        RegistroServ()
+            .registro(context, username, email, password, 3)
+            .then((value) {
+          Navigator.pop(
+              context, MaterialPageRoute(builder: (context) => const Login()));
+        });
       },
       child: SizedBox(
-          width: Responsive(context).wp(30), height: Responsive(context).hp(7), 
+          width: Responsive(context).wp(30),
+          height: Responsive(context).hp(7),
           child: const Center(child: Text('Registrarse'))),
     );
   }
--- lib/services/registro_serv.dart
+++ lib/services/registro_serv.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:telemetria/api/configure_api.dart';
 import 'package:telemetria/pages/registro_login.dart';
 import 'package:telemetria/utils/message.dart';
 import '../models/registroLog.dart';
@@ -8,45 +9,47 @@ import '../providers/registro_provider.dart';
 
 class RegistroServ {
   static final RegistroServ _registroServ = RegistroServ._internal();
-  final Dio _dio = Dio();
 
-  factory RegistroServ(){
+
+  factory RegistroServ() {
     return _registroServ;
   }
-  RegistroServ._internal(){
-    _dio.options.baseUrl = 'http://infopro-api.swplus.com.mx/api/auth/';
-    _dio.options.connectTimeout = 120000;
-    _dio.options.receiveTimeout = 120000;
-    _dio.options.receiveDataWhenStatusError = false;
+  RegistroServ._internal();
+  final Dio _dio = Dio(ConfigureApi.options);
 
-    _dio.options.responseType = ResponseType.plain;
+  Future<RegistroLog?> registro(BuildContext context, String username,
+      String email, String password, int roleId) async {
+    final registroLog = Provider.of<RegistroProvider>(context, listen: false);
+
+    try {
+      Message.show(context);
+      final response = await _dio.request('register',
+          data: {
+            "username": username,
+            "password": password,
+            "email": email,
+            "role_id": roleId
+          },
+          options: Options(method: 'POST'));
+
+      print(response.data);
+
+      if (response.statusCode == 200) {
+        final RegistroLog registroLog = RegistroLog.fromJson(response.data);
+        Message.dissmiss(context);
+      } else {
+        Message.dissmiss(context);
+        return null;
+
+      }
+    } on DioError catch (e) {
+      if (e.response != null) {
+        print(e.response?.statusCode);
+        print(e.response?.data);
+        Message.dissmiss(context);
+
+        return null;
+      }
+    }
   }
-
-  Future<RegistroLogin?> registro(BuildContext context, String username,
-      String email, String password) async {
-        final registroLog = Provider.of<RegistroProvider>(context, listen: false);
-      
-      try{
-        Message.show(context);
-        final response = await _dio.request('register',
-        data: {"username": username, "email": email, "password": password},
-        options: Options(method: 'POST'));
-
-        print(response.data);
-
-        if (response.statusCode == 200){
-          final RegistroLog registroLog = RegistroLog.fromJson(response.data);
-        } else {
-          return null;
-        }
-      } on DioError catch (e){
-        if (e.response != null){
-          print(e.response?.statusCode);
-          print(e.response?.data);
-          Message.dissmiss(context);
-
-          return null;
-        }
-      }
-      }
 }
